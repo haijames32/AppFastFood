1c459c3bfbecec0f28d8b0cb6e8f5af2980efd80
thuan <thuanndph26186@fpt.edu.vn>
2023-10-19 23:40:07 +0700

    thuan

---
+++ app/src/main/java/hainb21127/poly/appfastfood/adapter/ListDsAdapter.java
@@ -0,0 +1,72 @@
+package hainb21127.poly.appfastfood.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import hainb21127.poly.appfastfood.R;
+import hainb21127.poly.appfastfood.model.Product;
+
+public class ListDsAdapter extends RecyclerView.Adapter<ListDsAdapter.MyViewHolder>{
+    private List<Product> mProducts;
+    private Context context;
+
+    public ListDsAdapter(Context context) {
+        this.context = context;
+    }
+    public void setData(List<Product> arrayList){
+        this.mProducts = arrayList;
+        notifyDataSetChanged();
+    }
+    public ListDsAdapter(List<Product> mProducts) {
+        this.mProducts = mProducts;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_item_product_home,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+   Product product = mProducts.get(position);
+   if (product == null){
+       return;
+   }
+   holder.tv_item_name.setText(product.getTensp());
+   holder.tv_item_price.setText(product.getGiasp()+"");
+   Picasso.get().load(product.getImage()).into(holder.id_image);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mProducts!= null){
+            return mProducts.size();
+        }
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+   private ImageView id_image;
+   private TextView tv_item_name;
+   private TextView tv_item_price;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tv_item_name = itemView.findViewById(R.id.tv_item_name);
+            tv_item_price = itemView.findViewById(R.id.tv_item_price);
+            id_image = itemView.findViewById(R.id.item_img);
+        }
+    }
+}
--- app/src/main/java/hainb21127/poly/appfastfood/fragment/HomeFragment.java
+++ app/src/main/java/hainb21127/poly/appfastfood/fragment/HomeFragment.java
@@ -1,14 +1,32 @@
 package hainb21127.poly.appfastfood.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import hainb21127.poly.appfastfood.R;
+import hainb21127.poly.appfastfood.adapter.ListDsAdapter;
+import hainb21127.poly.appfastfood.database.FirebaseDB;
+import hainb21127.poly.appfastfood.model.Product;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,50 +35,75 @@ import hainb21127.poly.appfastfood.R;
  */
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
+    public static HomeFragment newInstance() {
         HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+    RecyclerView rcv_recommended;
+     ListDsAdapter adapter;
+    Context context;
+     List<Product> mpProducts;
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rcv_recommended = view.findViewById(R.id.rcv_recommended);
+        mpProducts = new ArrayList<>();
+        adapter = new ListDsAdapter(context);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,false);
+        rcv_recommended.setLayoutManager(linearLayoutManager);
+        getListProduct();
+    }
+    private void getListProduct(){
+        FirebaseDatabase database = FirebaseDB.getDatabaseInstance();
+        DatabaseReference myref = database.getReference("products");
+//        myref.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//
+//                String image = snapshot.child("image").getValue(String.class);
+//                String tensp = snapshot.child("tensp").getValue(String.class);
+//                String giasp = snapshot.child("giasp").getValue(String.class);
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+        myref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+              for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+              Product product = dataSnapshot.getValue(Product.class);
+              mpProducts.add(product);
+              adapter.setData(mpProducts);
+              rcv_recommended.setAdapter(adapter);
+               }
+              adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(context, "faild", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
