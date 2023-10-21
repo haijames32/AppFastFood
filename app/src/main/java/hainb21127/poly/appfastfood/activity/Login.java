656e6777cf1268dc3a5e183e366eedc37de9afe5
haijames32 <feckhai2003@gmail.com>
2023-10-21 23:54:53 +0700

    update

--- app/src/main/java/hainb21127/poly/appfastfood/activity/Login.java
+++ app/src/main/java/hainb21127/poly/appfastfood/activity/Login.java
@@ -7,9 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -42,9 +40,9 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        tvRegister = findViewById(R.id.tv_register_login);
         btn_signin_google = findViewById(R.id.btn_signin_google);
 
+        tvRegister = findViewById(R.id.tv_register_login);
 
         // Tạo client Google Sign In
         mGoogleSignInClient = GoogleSignIn.getClient(this, new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
