39e984ea5f1ab817cc44719461f7dfa53d4d249a
thuan <thuanndph26186@fpt.edu.vn>
2023-10-30 23:57:48 +0700

    thuan

--- app/src/main/java/hainb21127/poly/appfastfood/activity/ChangePass.java
+++ app/src/main/java/hainb21127/poly/appfastfood/activity/ChangePass.java
@@ -1,16 +1,99 @@
 package hainb21127.poly.appfastfood.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import hainb21127.poly.appfastfood.R;
 
 public class ChangePass extends AppCompatActivity {
-
+TextInputEditText ed_edit_pass, ed_edit_passnew, ed_edit_changepass;
+ImageView btn_back;
+Button btn_save;
+String mk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_pass);
+        ed_edit_pass = findViewById(R.id.ed_edit_pass);
+        btn_back = findViewById(R.id.img_back_chagepass);
+        ed_edit_passnew = findViewById(R.id.ed_edit_passnew);
+        ed_edit_changepass = findViewById(R.id.ed_edit_checkpass);
+        btn_save = findViewById(R.id.btn_edit_save_profile);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                String iduser = user.getUid();
+                String pass = ed_edit_pass.getText().toString().trim();
+                String passnew = ed_edit_passnew.getText().toString().trim();
+                String checkpass = ed_edit_changepass.getText().toString().trim();
+                FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+                DatabaseReference reference1 = firebaseDatabase.getReference("users").child(iduser);
+                reference1.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                         mk =snapshot.child("passwd").getValue(String.class);
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                    }
+                });
+                if (pass.isEmpty() ){
+                    ed_edit_pass.setError("không được để trống ");
+                } else if (passnew.isEmpty()) {
+                    ed_edit_passnew.setError("Không được để trống ");
+                } else if (checkpass.isEmpty()) {
+                    ed_edit_changepass.setError("không được để trống");
+                } else if (!passnew.equals(checkpass)) {
+                    ed_edit_changepass.setError("Chưa trùng mật khẩu");
+                } else if (!pass.equals(mk)) {
+                    ed_edit_pass.setError("Mật khẩu không tồn tại");
+                } else {
+                    user.updatePassword(passnew).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+                                DatabaseReference reference = firebaseDatabase.getReference("users").child(iduser);
+                                DatabaseReference reference1 = reference.child("passwd");
+                                reference1.setValue(passnew).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        Toast.makeText(ChangePass.this, "Đổi mật khẩu thành công", Toast.LENGTH_SHORT).show();
+                                        onBackPressed();
+                                    }
+                                });
+                            } else {
+                                Toast.makeText(ChangePass.this, "Đổi mật khẩu thất bại", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 }
--- app/src/main/java/hainb21127/poly/appfastfood/activity/EditProfile.java
+++ app/src/main/java/hainb21127/poly/appfastfood/activity/EditProfile.java
@@ -1,16 +1,113 @@
 package hainb21127.poly.appfastfood.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import hainb21127.poly.appfastfood.R;
+import hainb21127.poly.appfastfood.model.User_Register;
 
 public class EditProfile extends AppCompatActivity {
+    TextInputEditText ed_edit_name, ed_edit_phone, ed_edit_address ;
+    ImageView img_back;
+    Button btn_save;
+    FirebaseDatabase database;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        img_back = findViewById(R.id.img_back);
+        ed_edit_name = findViewById(R.id.ed_edit_fullname);
+        ed_edit_phone = findViewById(R.id.ed_edit_phone);
+        ed_edit_address = findViewById(R.id.ed_edit_address);
+        btn_save = findViewById(R.id.btn_edit_save_profile);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        database = FirebaseDatabase.getInstance();
+        String id = user.getUid();
+        reference = database.getReference("users").child(id);
+        if (user != null) {
+            reference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        User_Register userRegister = snapshot.getValue(User_Register.class);
+                        String name = userRegister.getFullname();
+                        int phone = userRegister.getPhone();
+                        String address = userRegister.getAddress();
+                        ed_edit_name.setText(name);
+                        ed_edit_phone.setText("0" + phone);
+                        ed_edit_address.setText(address);
+                    } else {
+                        Log.i("TAG", "onDataChange: Không lấy được thông tin người dùng");
+                    }
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Log.i("TAG", "onCancelled: " + error.toString());
+                }
+            });
+            btn_save.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String fullname = ed_edit_name.getText().toString();
+                    int phone = Integer.parseInt(ed_edit_phone.getText().toString());
+                    String address = ed_edit_address.getText().toString();
+                    if (fullname.isEmpty()) {
+                        ed_edit_name.setError("Không để trống");
+                    }else if (TextUtils.isEmpty(phone+"")) {
+                        ed_edit_phone.setError("Không để trống");
+                    }
+                    else if (address.isEmpty()) {
+                        ed_edit_address.setError("Không để trống");
+                    } else {
+                        Map<String, Object> updates = new HashMap<>();
+                        updates.put("fullname", fullname);
+                        updates.put("phone", phone);
+                        updates.put("address", address);
+                        reference.updateChildren(updates).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(EditProfile.this, "Cập nhật thông tin thành công", Toast.LENGTH_SHORT).show();
+                                    onBackPressed();
+                                } else {
+                                    Toast.makeText(EditProfile.this, "Cập nhật thông tin thất bại", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                }
+            });
+            img_back.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onBackPressed();
+                }
+            });
+
+        }
     }
 }
